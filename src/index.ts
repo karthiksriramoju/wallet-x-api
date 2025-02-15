278614e47ff85ff51afe5438aa6d58e9d4efbc6e
Your Name <karthiksriramoju11@gmail.com>
2025-02-15 10:16:20 +0530

    added get req

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,10 @@ const PORT = parseInt(process.env.PORT || '3000') || 3000;
 // Connect to MongoDB
 connectDB();
 
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
